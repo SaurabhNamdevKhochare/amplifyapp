6d8eb5e53d6ae15d1d3ac3ee182f3e0f98c72d91
SaurabhNamdevKhochare <110380823+SaurabhNamdevKhochare@users.noreply.github.com>
2022-08-03 17:04:15 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+function sayHello() {
+  alert("Hello!");
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello Saurabh Code Sucessfully deployed on AWS.</h1>
+        <button type='button' className='button' onClick={() => sayHello()}>Click This Button!</button>
         {/* <a
           className="App-link"
           href="https://reactjs.org"
